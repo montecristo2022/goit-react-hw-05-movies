516dcff804c94ffd904be3e8418306760f16a1b9
montecristo2022 <104501641+montecristo2022@users.noreply.github.com>
2022-12-09 10:59:58 +0200

    save

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-  <BrowserRouter>
+  <BrowserRouter basename='/goit-react-hw-05-movies/'>
       <App />
   </BrowserRouter>
 
